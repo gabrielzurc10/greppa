7f6f306c84740bcef04333f3151d941d63dc6704
Gabriel Cruz <Gabriel102697_cruz@yahoo.com>
2026-07-08 17:27:57 -0500

    add storage account name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "greppa-tfstate-rg"
-    storage_account_name = "REPLACED_BY_BOOTSTRAP"
+    storage_account_name = "greppatff2b7d6c23e054a"
     container_name       = "tfstate"
     key                  = "greppa.tfstate"
   }
